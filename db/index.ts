b3e4d970cc0a7cc10890ec956f6c89abb2aabc02
vr1Ravi <ravikumarjha059@gmail.com>
2024-04-22 23:30:03 +0530

    rollback prismaClient

--- db/index.ts
+++ db/index.ts
@@ -1,15 +1,18 @@
-// @ts-nocheck
-import { PrismaClient } from './prisma/generated/client';
+import { PrismaClient } from '@prisma/client';
 
-let prisma: PrismaClient;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+// eslint-disable-next-line
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
