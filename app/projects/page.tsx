c6fdd6cc06beeeb9efbf3bf2fb2412932da8a20a
vr1Ravi <ravikumarjha059@gmail.com>
2024-04-22 16:47:37 +0530

    add eslint

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,12 @@
 import Heading from '@/app/ui/heading';
 import ScrollToTop from '@/app/ui/projects/scrolltotop';
 import { addProject, getProjects } from '@/app/lib/data';
+import { projectType } from '@/types/types';
 // import { newProject } from '@/app/lib/seed';
 import Project from '../ui/projects/project';
 export default async function Projects() {
   // await addProject(newProject);
-  const projects = (await getProjects()).reverse();
+  const projects: projectType[] = (await getProjects()).reverse();
 
   return (
     <main className="mt-12 flex w-full flex-col justify-between p-3">
